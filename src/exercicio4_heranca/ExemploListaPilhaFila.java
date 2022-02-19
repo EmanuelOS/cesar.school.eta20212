3bb4f666133c21223b2c0ae3270d2b9e6e302d8a
EmanuelOS <silva.oliveira.emanuel@gmail.com>
2022-02-19 08:58:31 -0300

    Initial
    ETA2021.2 Aula 4 - manhã

---
+++ src/exercicio4_heranca/ExemploListaPilhaFila.java
@@ -0,0 +1,10 @@
+package exercicio4_heranca;
+
+public class ExemploListaPilhaFila {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/exercicio4_heranca/VeiculoAreo.java
@@ -0,0 +1,13 @@
+package exercicio4_heranca;
+
+public interface VeiculoAreo {
+	
+	public void planar();
+	
+	public void ligarTurbina();
+	
+	public void decolar();
+	
+	public void pousar();
+
+}
